5ce5410d511c27c8b0227a4fd4be6e331e2d8480
Hoós Roland <shadeeygg@gmail.com>
2022-10-07 16:55:38 +0200

    Bolygó

--- src/hu/petrik/enumok/Main.java
+++ src/hu/petrik/enumok/Main.java
@@ -1,7 +1,11 @@
 package hu.petrik.enumok;
 
+import hu.petrik.enumok.bolygo.Bolygo;
+
 public class Main {
     public static void main(String[] args) {
-        
+        for (Bolygo bolygo: Bolygo.values()){
+            System.out.println(bolygo);
+        }
     }
 }
